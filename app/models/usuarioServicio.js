bf013c952d50fa24c18b5e9aaf9018458314cd79
ProyectoUncsm <proyectouncsm@gmail.com>
2025-05-09 00:27:19 -0600

    Delete app directory

--- app/models/usuarioServicio.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('UsuarioServicio', {
-      numero_cuenta: DataTypes.STRING,
-      estado: DataTypes.STRING,
-      creado_en: DataTypes.DATE
-    });
-  };
-  
--- app/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { realizarPago } = require('../controllers/paymentController');
-const auth = require('../middleware/authMiddleware');
-
-router.post('/realizar', auth, realizarPago);
-
-module.exports = router;
