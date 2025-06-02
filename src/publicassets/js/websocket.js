086e34016302e514116ce1cfe56b9f894ccb07d0
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-01 22:14:54 -0600

    prueba websocket

--- src/publicassets/js/websocket.js
+++ src/publicassets/js/websocket.js
@@ -21,8 +21,6 @@ socket.on('estadoServicioActualizado', (data) => {
   // location.reload() o modificá el DOM
 });
 
-const socket = io();
-
 // Escuchar evento del servidor
 socket.on('servicioActualizado', (data) => {
   const usuarioLogueadoId = parseInt(document.getElementById('correo').value);
