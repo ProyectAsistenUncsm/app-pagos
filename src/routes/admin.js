35bc80b4e9dd226460bf861391cc10a02fec5dde
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-03 06:31:03 -0600

    prueba websocket

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router();
-const Pago = require('../models/Pago');
+import {Pay} from '../models/Pago';
 
+const router = express.Router();
 // Historial de pagos
-router.get('/admin/historial-pagos', async (req, res) => {
+router.get('/admin/', async (req, res) => {
   try {
     const pagos = await Pago.find().sort({ fecha: -1 });
-    res.render('historial', { pagos });
+    res.render('historial', { Pay });
   } catch (err) {
     res.status(500).send('Error al obtener historial');
   }
