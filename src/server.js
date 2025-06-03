106087255fd2d96a21dcce26ab9a3a07a24ef678
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-02 20:21:51 -0600

    prueba websocket

--- src/server.js
+++ src/server.js
@@ -17,13 +17,6 @@ const servicioRoutes = require('../routes/servicios');
 // Inicializa la app
 const app = express();
 const server = http.createServer(app);
-//const io = socketIO(server);
-
-// Seguridad y middlewares
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // Socket.IO
 const { Server } = require('socket.io');
@@ -43,6 +36,13 @@ io.on('connection', (socket) => {
     console.log('🔴 Usuario desconectado', socket.id);
   });
 });
+
+// Seguridad y middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Motor de vistas EJS
 app.set('view engine', 'ejs');
