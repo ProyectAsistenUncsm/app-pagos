10d66eed9551c04846f868f2aaaf55bb42bb6d7b
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-04 23:11:50 -0600

    act  db

--- config/db.js
+++ config/db.js
@@ -7,28 +7,18 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-);
+});
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false
@@ -36,3 +26,4 @@ const pool = new Pool({
 });
 
 export { sequelize, pool };
+
