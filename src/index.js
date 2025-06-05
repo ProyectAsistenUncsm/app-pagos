f77f6fc931f335aa98c88563eb91089130e58cbb
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-04 22:05:03 -0600

    prueba index act - 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/authRoutes.js';
 import indexRoutes from './routes/indexRoutes.js';
 import { User } from './models/indexModel.js';
@@ -12,19 +16,19 @@ import { User } from './models/indexModel.js';
 
 //app.use('/', routes_user);
 
+
+const app = express();
+
 //directory of files
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Middleware básico
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(join(__dirname, 'publicassets')));
-
-// Configuración de vistas
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(indexRoutes);
+app.use('/auth', indexRoutes);
+
+app.use(express.static(join(__dirname, 'publicassets')));
+
 
 // Middleware para agregar datos del usuario a las vistas
 app.use(async (req, res, next) => {
@@ -44,17 +48,7 @@ app.use(async (req, res, next) => {
   next();
 });
 
-// Rutas
-app.use('/auth', authRoutes);
-app.use('/', indexRoutes);
 
-// Manejo de errores 404
-app.use((req, res) => {
-  res.status(404).render('404', { 
-    mensaje: 'Página no encontrada',
-    usuario: req.user 
-  });
-});
 
 app.listen(process.env.PORT || 3000);
 console.log('Server is running on port', 3000);
