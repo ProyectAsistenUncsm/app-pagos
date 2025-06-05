cde18d95b5b58300fa61f250cfce05cbb0204917
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-04 22:13:31 -0600

    prueba index act - abilit-indexroutes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import authRoutes from './routes/authRoutes.js';
-//import indexRoutes from './routes/indexRoutes.js';
+//import authRoutes from './routes/authRoutes.js';
+import indexRoutes from './routes/indexRoutes.js';
 //import { User } from './models/indexModel.js';
 // const jwt = require('jsonwebtoken');
 //const routes_user = require('./routes/userRoutes.js');
