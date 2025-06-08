ffc10b6c857604b17b4d061f062e5e9acdebe4ed
ProyectoUncsm <proyectouncsm@gmail.com>
2025-06-07 21:39:13 -0600

    ch

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import User from '../models/userModel.js';
 import { verificarToken, esAdmin } from '../middleware/authMiddleware.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { obtenerHistorialPagos } from '../controllers/paymentController.js';
 
 const router = Router();
